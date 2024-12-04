c308cd8eb16f4129a6987cd812e4fbade22b3c62
Sasilk <83803923+Sasilk@users.noreply.github.com>
2024-12-04 00:55:34 +0000

    Update and rename GetComputerListFromAD to GetComputerListFromAD.ps1

--- GetComputerListFromAD.ps1
+++ GetComputerListFromAD.ps1
@@ -1,6 +1,5 @@
-It is handful to be able to get a list of computers in a particular OU in AD, you can use the PowerShell script below I created.
-
-Please note you need to edit it with your OU name. Basically copy the lines below in a txt file, amend the OU name and save it as GetComputerListFromAD.ps1. And run.
+# It is handful to be able to get a list of computers in a particular OU in AD, you can use the PowerShell script below I created.
+# Please note you need to edit it with your OU name. Basically copy the lines below in a txt file, amend the OU name and save it as GetComputerListFromAD.ps1. And run.
 
 # Define the OU from which to retrieve computers. Please note OUs here is in reverse order. You can copy&paste the OU Distinguished name from AD — Right click on AD and Properties > Attribute Editor and find distinguishedName
 $ou = “OU=A1,OU=ATeams,OU=ADepartment,OU=Departments,OU=Computer Locations,DC=xyz,DC=local”
