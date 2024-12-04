8c6a95fff991b5b105d06779d205c740c8bcb527
Sasilk <83803923+Sasilk@users.noreply.github.com>
2024-12-04 00:54:47 +0000

    Update and rename GetADGroupsSyncingTo365 to GetADGroupsSyncingTo365.ps1

--- GetADGroupsSyncingTo365.ps1
+++ GetADGroupsSyncingTo365.ps1
@@ -1,2 +1,2 @@
-- To export a list of groups syncing to 365 via AAD Connect (eg. Directory Sync Enabled)
+# To export a list of groups syncing to 365 via AAD Connect (eg. Directory Sync Enabled)
 Get-AzureADGroup | Where-Object { $_.DirSyncEnabled -eq $true } | Export-Csv -Path "C:\temp\ExportedData\SecurityGroupsSyncedFromADs.csv"
